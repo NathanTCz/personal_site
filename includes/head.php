737db5ffea9660240eb875b94e046df5ee9f1d65
NathanTcz <nathancazell@gmail.com>
2015-03-24 13:54:02 -0400

    fix paths for production server

--- includes/head.php
+++ includes/head.php
@@ -1,18 +1,18 @@
 <head>
     <meta charset="utf-8"/>
-	<title>Nathan Cazell</title>
-	
-	<link rel="shortcut icon" href="/images/paper.png" type="image/x-icon" />
+  <title>Nathan Cazell</title>
 
-	<link rel="stylesheet" type="text/css" href="css/main.css"/>
-	
-	<script>
-		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  <link rel="shortcut icon" href="images/paper.png" type="image/x-icon" />
 
-		ga('create', 'UA-39946518-1', 'nathancazell.com');
-		ga('send', 'pageview');
-	</script>
+  <link rel="stylesheet" type="text/css" href="css/main.css"/>
+
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-39946518-1', 'ww2.cs.fsu.edu/~cazell');
+    ga('send', 'pageview');
+  </script>
 </head>
